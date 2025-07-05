cbf1c9ad2f400b92a969d2b758d985d07ae22ab1
polyalugovenko <polinalugovenko0403@gmail.com>
2025-07-05 20:34:12 +0300

    fix: add validate method

--- src/main/java/ru/eventorg/service/EventService.java
+++ src/main/java/ru/eventorg/service/EventService.java
@@ -2,9 +2,13 @@ package ru.eventorg.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import ru.eventorg.entity.EventEntity;
 import ru.eventorg.exception.ErrorState;
+import ru.eventorg.exception.EventNotActiveException;
 import ru.eventorg.exception.EventNotExistException;
 import ru.eventorg.repository.EventEntityRepository;
 
@@ -40,5 +44,25 @@ public class EventService {
                             .map((row, meta) -> row.get("event_status_name", String.class))
                             .first();
                 });
+    }
+
+    public Mono<Void> validateExists(Integer eventId) {
+        return eventEntityRepository.existsById(eventId)
+                .flatMap(exists -> exists
+                        ? Mono.empty()
+                        : Mono.error(new EventNotExistException(ErrorState.EVENT_NOT_EXIST)));
+    }
+
+
+    public Mono<EventEntity> getActiveOrCompletedEvent(Integer eventId) {
+        return eventEntityRepository.getActiveOrCompletedEventById(eventId)
+                .switchIfEmpty(Mono.error(new EventNotExistException(ErrorState.EVENT_NOT_EXIST)));
+    }
+
+    public Mono<Void> validateEventIsActive(Integer eventId) {
+        return eventEntityRepository.existsActiveEventById(eventId)
+                .flatMap(isActive -> isActive
+                        ? Mono.empty()
+                        : Mono.error(new EventNotActiveException(ErrorState.EVENT_NOT_ACTIVE)));
     }
 }
