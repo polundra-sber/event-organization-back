a3d467765b010dc05518a61d27f78a51b8372332
StanleyStanMarsh <iastafev56@gmail.com>
2025-07-05 12:39:45 +0300

    feat: add getUserMetadata method

---
+++ src/main/java/ru/eventorg/controller/UserMetadataController.java
@@ -0,0 +1,36 @@
+package ru.eventorg.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.openapitools.api.UserMetadataApi;
+import org.openapitools.model.EventUserMetadata;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import ru.eventorg.security.SecurityUtils;
+import ru.eventorg.service.EventService;
+import ru.eventorg.service.RoleService;
+
+@RestController
+@RequiredArgsConstructor
+public class UserMetadataController implements UserMetadataApi {
+    private final RoleService roleService;
+    private final EventService eventService;
+
+    @Override
+    public Mono<ResponseEntity<EventUserMetadata>> getUserMetadata(Integer eventId, ServerWebExchange exchange) throws Exception {
+        return SecurityUtils.getCurrentUserLogin()
+                .flatMap(login ->
+                        Mono.zip(
+                                roleService.getUserRoleInEvent(eventId, login),
+                                eventService.getEventStatus(eventId),
+                                (roleName, eventStatus) -> {
+                                    EventUserMetadata meta = new EventUserMetadata();
+                                    meta.setRoleName(roleName);
+                                    meta.setEventStatusName(eventStatus);
+                                    return ResponseEntity.ok(meta);
+                                }
+                        )
+                );
+    }
+}
--- src/main/java/ru/eventorg/service/RoleService.java
+++ src/main/java/ru/eventorg/service/RoleService.java
@@ -5,15 +5,18 @@ import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import ru.eventorg.exception.ErrorState;
+import ru.eventorg.exception.EventNotExistException;
 import ru.eventorg.exception.UserNotEventParticipantException;
 import ru.eventorg.exception.WrongUserRoleException;
+import ru.eventorg.repository.EventEntityRepository;
 
 @Service
 @RequiredArgsConstructor
 public class RoleService {
     private final R2dbcEntityTemplate template;
+    private final EventEntityRepository eventEntityRepository;
 
-    private final String sqlCheckRole = """
+    private final String SQL_CHECK_ROLE = """
             SELECT role.role_name
             FROM event_user_list eul
             JOIN role ON eul.role_id = role.role_id
@@ -21,36 +24,52 @@ public class RoleService {
             """;
 
     public Mono<Boolean> checkIfCreator(Integer eventId, String login) {
-        return template.getDatabaseClient()
-                .sql(sqlCheckRole)
-                .bind(0, eventId)
-                .bind(1, login)
-                .map(row -> row.get("role_name", String.class))
-                .first()
-                .switchIfEmpty(Mono.error(new UserNotEventParticipantException(ErrorState.USER_NOT_EVENT_PARTICIPANT)))
+        return getUserRoleInEvent(eventId, login)
                 .flatMap(roleName -> {
                     if ("Создатель".equalsIgnoreCase(roleName)) {
                         return Mono.just(true);
-                    } else {
-                        return Mono.error(new WrongUserRoleException(ErrorState.NOT_CREATOR_ROLE));
                     }
+                    return Mono.error(new WrongUserRoleException(ErrorState.NOT_CREATOR_ROLE));
                 });
     }
 
     public Mono<Boolean> checkIfOrganizerOrHigher(Integer eventId, String login) {
-        return template.getDatabaseClient()
-                .sql(sqlCheckRole)
-                .bind(0, eventId)
-                .bind(1, login)
-                .map(row -> row.get("role_name", String.class))
-                .first()
-                .switchIfEmpty(Mono.error(new UserNotEventParticipantException(ErrorState.USER_NOT_EVENT_PARTICIPANT)))
+        return getUserRoleInEvent(eventId, login)
                 .flatMap(roleName -> {
-                    if ("Организатор".equalsIgnoreCase(roleName) || "Создатель".equalsIgnoreCase(roleName)) {
+                    if ("Организатор".equalsIgnoreCase(roleName)
+                            || "Создатель".equalsIgnoreCase(roleName)) {
                         return Mono.just(true);
-                    } else {
-                        return Mono.error(new WrongUserRoleException(ErrorState.NOT_CREATOR_OR_ORGANIZER_ROLE));
                     }
+                    return Mono.error(new WrongUserRoleException(
+                            ErrorState.NOT_CREATOR_OR_ORGANIZER_ROLE));
+                });
+    }
+
+    /**
+     * Возвращает роль пользователя в рамках указанного мероприятия.
+     * Если пользователь не участвует в событии, бросает UserNotEventParticipantException,
+     * если мероприятия не существует, бросает EventNotExistException
+     *
+     * @param eventId идентификатор мероприятия
+     * @param login   логин пользователя
+     * @return Mono с названием роли
+     */
+    public Mono<String> getUserRoleInEvent(Integer eventId, String login) {
+        return eventEntityRepository.existsEventEntityByEventId(eventId)
+                .flatMap(exists -> {
+                    if (!exists) {
+                        return Mono.error(new EventNotExistException(ErrorState.EVENT_NOT_EXIST));
+                    }
+                    return template.getDatabaseClient()
+                            .sql(SQL_CHECK_ROLE)
+                            .bind(0, eventId)
+                            .bind(1, login)
+                            .map((row, meta) -> row.get("role_name", String.class))
+                            .first()
+                            .switchIfEmpty(
+                                    Mono.error(new UserNotEventParticipantException(
+                                            ErrorState.USER_NOT_EVENT_PARTICIPANT))
+                            );
                 });
     }
 }
