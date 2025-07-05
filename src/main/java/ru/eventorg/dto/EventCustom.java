14135cc9dc147c68ba3aed908522fb6fbe91909f
polyalugovenko <polinalugovenko0403@gmail.com>
2025-07-05 17:01:28 +0300

    feat: custom dto created

---
+++ src/main/java/ru/eventorg/dto/EventCustom.java
@@ -0,0 +1,23 @@
+package ru.eventorg.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventCustom {
+    private Integer eventId;
+    private String eventName;
+    private String eventDescription;
+    private String status;
+    private String location;
+    private LocalDate eventDate;
+    private LocalTime eventTime;
+    private String chatLink;
+    private String role;
+}
---
+++ src/main/java/ru/eventorg/dto/EventPreviewCustom.java
@@ -0,0 +1,20 @@
+package ru.eventorg.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventPreviewCustom {
+    private Integer eventId;
+    private String eventName;
+    private String status;
+    private String location;
+    private LocalDate eventDate;
+    private LocalTime eventTime;
+}
