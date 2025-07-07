81cea21e38b1c463cce11fa06b796165f8e77604
polyalugovenko <polinalugovenko0403@gmail.com>
2025-07-07 17:37:14 +0300

    feat: ProfileApi created

--- src/main/java/ru/eventorg/repository/UserProfilesEntityRepository.java
+++ src/main/java/ru/eventorg/repository/UserProfilesEntityRepository.java
@@ -1,6 +1,9 @@
 package ru.eventorg.repository;
 
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.data.repository.query.Param;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.eventorg.entity.UserProfileEntity;
@@ -10,4 +13,11 @@ import java.util.Set;
 public interface UserProfilesEntityRepository extends R2dbcRepository<UserProfileEntity, String> {
     Mono<UserProfileEntity> findByLogin(String login);
     Flux<UserProfileEntity> findByLoginIn(Set<String> logins);
+
+    @Modifying
+    @Query("UPDATE user_profile SET name = :name, surname = :surname, comment_money_transfer = :comment WHERE login = :login")
+    Mono<Integer> updateProfile(@Param("login") String login,
+                                @Param("name") String name,
+                                @Param("surname") String surname,
+                                @Param("comment") String comment);
 }
--- src/main/java/ru/eventorg/repository/UserSecretsEntityRepository.java
+++ src/main/java/ru/eventorg/repository/UserSecretsEntityRepository.java
@@ -1,6 +1,9 @@
 package ru.eventorg.repository;
 
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.data.repository.query.Param;
 import reactor.core.publisher.Mono;
 import ru.eventorg.entity.UserSecretEntity;
 
@@ -8,4 +11,12 @@ public interface UserSecretsEntityRepository extends R2dbcRepository<UserSecretE
     Mono<UserSecretEntity> findByLogin(String login);
     Mono<Boolean> existsByLogin(String login);
     Mono<Boolean> existsByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE user_secret SET email = :email WHERE login = :login")
+    Mono<Integer> updateEmail(@Param("login") String login, @Param("email") String email);
+
+    @Query("SELECT COUNT(*) > 0 FROM user_secret WHERE email = :email AND login != :login")
+    Mono<Boolean> existsByEmailAndLoginNot(@Param("email") String email,
+                                           @Param("login") String login);
 }
