daea4649126d9c793a8b9745f59ea331b60da229
polyalugovenko <polinalugovenko0403@gmail.com>
2025-07-09 14:25:43 +0300

    fix: correct re-joining processing

--- src/main/java/ru/eventorg/repository/RoleEntityRepository.java
+++ src/main/java/ru/eventorg/repository/RoleEntityRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Mono;
 import ru.eventorg.entity.RoleEntity;
 
+import java.util.List;
+
 public interface RoleEntityRepository extends R2dbcRepository<RoleEntity, Integer> {
     Mono<RoleEntity> getRoleEntityByRoleId(Integer roleId);
+    Mono<Integer> getRoleIdByRoleName(String roleName);
 }
